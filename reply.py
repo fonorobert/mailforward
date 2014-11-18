15b3e04e4c6ab5119f2c649071e06fdf85101169
fonorobert <fonorobert@gmail.com>
2014-11-18 20:18:45 +0100

    tests and tinkering

--- reply.py
+++ reply.py
@@ -36,7 +36,7 @@ this_address = incoming['to']
 if incoming.is_multipart():
     for payload in incoming.get_payload():
         # if payload.is_multipart(): ...
-        body = payload.get_payload()
+        body = payload.get_payload(decode=True)
 else:
     body = incoming.get_payload(decode=True)
     body = body.encode()
